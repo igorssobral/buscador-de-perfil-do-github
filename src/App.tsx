2c6c2282ed2bb2b1450127b65ef14cd2e0bfc438
Igor Sobral <igorgmz1421@gmail.com>
2023-12-07 18:14:06 -0300

    add: config router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import {Outlet} from "react-router-dom"
 
 function App() {
   return (
     <>
       <div className="App">
         <h1>GitHub Finder</h1>
+        <Outlet/>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import * as React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+
+import Home from './routes/Home'
+const router = createBrowserRouter([
+  { path: '/',
+    element: <App/>,
+    children:[
+  { path: '/',
+    element:<Home/>
+},
+    ]
+}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
